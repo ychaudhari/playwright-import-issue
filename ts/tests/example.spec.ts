692649047b1a2f67b5b2878c4f35c034ff717d8d
Yogesh Chaudhari <YChaudhari@linz.govt.nz>
2021-11-22 22:08:24 +1300

    ignore

--- ts/tests/example.spec.ts
+++ ts/tests/example.spec.ts
@@ -2,8 +2,8 @@
 import { test } from '@playwright/test';
 import { TestPage } from '../models/TestPage';
 
-test.describe('headed block', () => {
-    test('my headed test', async ({ page }) => {
+test.describe('test block', () => {
+    test('my ts test', async ({ page }) => {
         const testPage = new TestPage(page);
         await testPage.navigate();
     });
